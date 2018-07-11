0ec7d2db42752f13b1c365f99baa8fe99dc98808
Noel Cordova <ednoesco_7@hotmail.com>
2018-07-11 12:46:01 -0500

    update app-routing.module and delete .spec files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
